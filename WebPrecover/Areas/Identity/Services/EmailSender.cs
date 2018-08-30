b985174d77ce84475ddedcefed9a3509c9510e17
Andy Sheley <andrew.sheley@gmail.com>
2018-08-29 22:01:25 -0500

    Add project files.

---
+++ WebPrecover/Areas/Identity/Services/EmailSender.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPrecover.Areas.Identity.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration Configuraiton;
+
+        public EmailSender(IConfiguration configuraiton)
+        {
+            Configuraiton = configuraiton;
+        }
+
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //throw new NotImplementedException();
+            string apiKey = Configuraiton["SendGridAPIKey"];
+            
+            return Execute(apiKey, subject, htmlMessage, email);
+            
+        }
+
+
+        public Task Execute(string apiKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("Joe@contoso.com", "Joe Smith"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.TrackingSettings = new TrackingSettings
+            {
+                ClickTracking = new ClickTracking { Enable = false }
+            };
+
+            return client.SendEmailAsync(msg);
+        }
+
+
+    }
+}
